82c2a2d07c21daaa1f1cea8773d785214c9dc339
romanvog <romanvog@gmail.com>
2015-11-13 23:15:41 +0200

    1

--- src/Triviya/Main.java
+++ src/Triviya/Main.java
@@ -17,7 +17,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Wellcome to the Trivia Consol application");
         Scanner s=new Scanner(System.in);
         String op= "";
